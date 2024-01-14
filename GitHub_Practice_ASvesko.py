b69c7bfc91504c175d9ee041d12bdfa6291288d7
adriansvesko <156615102+adriansvesko@users.noreply.github.com>
2024-01-14 16:31:55 -0500

    Add files via upload

---
+++ GitHub_Practice_ASvesko.py
@@ -0,0 +1,6 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Creating a project for GitHub
+>>> #By Adrian Svesko
+>>> print("COP2002.0M2 Spring 2024")
+COP2002.0M2 Spring 2024
